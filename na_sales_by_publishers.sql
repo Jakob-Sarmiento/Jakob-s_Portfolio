b1ea88a803706a226bc32e7b94253507cea1e6fe
Jakob-Sarmiento <jakobnsarmiento@gmail.com>
2025-09-03 14:30:19 -0400

    Delete na_sales_by_publishers.sql

--- na_sales_by_publishers.sql
+++
@@ -1,8 +0,0 @@
-#3b — Top 5 publishers in North America
-SELECT
-  publisher,
-  ROUND(SUM(na_sales), 2) AS total_na_sales
-FROM `jakemoneyorg.vgchartz.analysis_ox`
-GROUP BY publisher
-ORDER BY total_na_sales DESC
-LIMIT 5;
