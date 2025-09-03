1582b4d7b47251d7e0833a39e783026bf9f9941a
Jakob-Sarmiento <jakobnsarmiento@gmail.com>
2025-09-03 14:53:34 -0400

    Delete 04_na_sales_by_titles.sql

--- 04_na_sales_by_titles.sql
+++
@@ -1,8 +0,0 @@
-#2b - Top 10 highest-selling titles in North America
-SELECT
-  title,
-  ROUND(SUM(na_sales), 2) AS total_na_sales
-FROM `jakemoneyorg.vgchartz.analysis_ox`
-GROUP BY title
-ORDER BY total_na_sales DESC
-LIMIT 10;
